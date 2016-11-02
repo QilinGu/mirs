a39728a8a008447f52cbdd766db1f41ecf094d75
邓 <1351650853@qq.com>
2016-11-02 17:42:36 +0800

    movie数据结构修改

--- src/main/java/com/kevin/mirs/entity/Movie.java
+++ src/main/java/com/kevin/mirs/entity/Movie.java
@@ -4,7 +4,10 @@ package com.kevin.mirs.entity;
 public class Movie {
 
     private String id;
+    private String douban_id;
     private String name;
+    private String doubanRating;
+    private String imdbRating;
     private String year;
     private String directors;
     private String screenwriters;
@@ -16,7 +19,8 @@ public class Movie {
     private String languages;
     private String runtime;
     private String anotherNames;
-    private String IMDbLink;
+    private String imdbLink;
+    private String coverLink;
     private String synopsis;
     private String stillsLink;
     private String stillsPhotosLinks;
@@ -34,251 +38,152 @@ public class Movie {
     private String shortNewComments;
     private String shortCommentsLink;
 
+
     public String getId() {
         return id;
     }
 
-    public void setId(String id) {
-        this.id = id;
+    public String getDouban_id() {
+        return douban_id;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public String getDoubanRating() {
+        return doubanRating;
+    }
+
+    public String getImdbRating() {
+        return imdbRating;
     }
 
     public String getYear() {
         return year;
     }
 
-    public void setYear(String year) {
-        this.year = year;
-    }
-
     public String getDirectors() {
         return directors;
-    }
-
-    public void setDirectors(String directors) {
-        this.directors = directors;
     }
 
     public String getScreenwriters() {
         return screenwriters;
     }
 
-    public void setScreenwriters(String screenwriters) {
-        this.screenwriters = screenwriters;
-    }
-
     public String getActors() {
         return actors;
-    }
-
-    public void setActors(String actors) {
-        this.actors = actors;
     }
 
     public String getTypes() {
         return types;
     }
 
-    public void setTypes(String types) {
-        this.types = types;
-    }
-
-    public String getOfficalWebsite() {
+    public String getOfficialWebsite() {
         return officialWebsite;
-    }
-
-    public void setOfficalWebsite(String officialWebsite) {
-        this.officialWebsite = officialWebsite;
     }
 
     public String getOriginPlace() {
         return originPlace;
     }
 
-    public void setOriginPlace(String originPlace) {
-        this.originPlace = originPlace;
-    }
-
     public String getReleaseDate() {
         return releaseDate;
-    }
-
-    public void setReleaseDate(String releaseDate) {
-        this.releaseDate = releaseDate;
     }
 
     public String getLanguages() {
         return languages;
     }
 
-    public void setLanguages(String languages) {
-        this.languages = languages;
-    }
-
     public String getRuntime() {
         return runtime;
-    }
-
-    public void setRuntime(String runtime) {
-        this.runtime = runtime;
     }
 
     public String getAnotherNames() {
         return anotherNames;
     }
 
-    public void setAnotherNames(String anotherNames) {
-        this.anotherNames = anotherNames;
+    public String getImdbLink() {
+        return imdbLink;
     }
 
-    public String getIMDbLink() {
-        return IMDbLink;
-    }
-
-    public void setIMDbLink(String IMDbLink) {
-        this.IMDbLink = IMDbLink;
+    public String getCoverLink() {
+        return coverLink;
     }
 
     public String getSynopsis() {
         return synopsis;
     }
 
-    public void setSynopsis(String synopsis) {
-        this.synopsis = synopsis;
-    }
-
     public String getStillsLink() {
         return stillsLink;
-    }
-
-    public void setStillsLink(String stillsLink) {
-        this.stillsLink = stillsLink;
     }
 
     public String getStillsPhotosLinks() {
         return stillsPhotosLinks;
     }
 
-    public void setStillsPhotosLinks(String stillsPhotosLinks) {
-        this.stillsPhotosLinks = stillsPhotosLinks;
-    }
-
     public String getPosterLink() {
         return posterLink;
-    }
-
-    public void setPosterLink(String posterLink) {
-        this.posterLink = posterLink;
     }
 
     public String getPosterPhotosLinks() {
         return posterPhotosLinks;
     }
 
-    public void setPosterPhotosLinks(String posterPhotosLinks) {
-        this.posterPhotosLinks = posterPhotosLinks;
-    }
-
     public String getWallpaperLink() {
         return wallpaperLink;
-    }
-
-    public void setWallpaperLink(String wallpaperLink) {
-        this.wallpaperLink = wallpaperLink;
     }
 
     public String getWallpaperPhotosLinks() {
         return wallpaperPhotosLinks;
     }
 
-    public void setWallpaperPhotosLinks(String wallpaperPhotosLinks) {
-        this.wallpaperPhotosLinks = wallpaperPhotosLinks;
-    }
-
     public String getAwards() {
         return awards;
-    }
-
-    public void setAwards(String awards) {
-        this.awards = awards;
     }
 
     public String getAlsoLikeMovies() {
         return alsoLikeMovies;
     }
 
-    public void setAlsoLikeMovies(String alsoLikeMovies) {
-        this.alsoLikeMovies = alsoLikeMovies;
-    }
-
     public String getQuestions() {
         return questions;
-    }
-
-    public void setQuestions(String questions) {
-        this.questions = questions;
     }
 
     public String getAllQuestionsLink() {
         return allQuestionsLink;
     }
 
-    public void setAllQuestionsLink(String allQuestionsLink) {
-        this.allQuestionsLink = allQuestionsLink;
-    }
-
     public String getReviews() {
         return reviews;
-    }
-
-    public void setReviews(String reviews) {
-        this.reviews = reviews;
     }
 
     public String getReviewsLink() {
         return reviewsLink;
     }
 
-    public void setReviewsLink(String reviewsLink) {
-        this.reviewsLink = reviewsLink;
-    }
-
     public String getShortPopComments() {
         return shortPopComments;
-    }
-
-    public void setShortPopComments(String shortPopComments) {
-        this.shortPopComments = shortPopComments;
     }
 
     public String getShortNewComments() {
         return shortNewComments;
     }
 
-    public void setShortNewComments(String shortNewComments) {
-        this.shortNewComments = shortNewComments;
-    }
-
     public String getShortCommentsLink() {
         return shortCommentsLink;
     }
 
-    public void setShortCommentsLink(String shortCommentsLink) {
-        this.shortCommentsLink = shortCommentsLink;
-    }
 
     @Override
     public String toString() {
         return "Movie{" +
                 "id='" + id + '\'' +
+                ", douban_id='" + douban_id + '\'' +
                 ", name='" + name + '\'' +
+                ", doubanRating='" + doubanRating + '\'' +
+                ", imdbRating='" + imdbRating + '\'' +
                 ", year='" + year + '\'' +
                 ", directors='" + directors + '\'' +
                 ", screenwriters='" + screenwriters + '\'' +
@@ -290,7 +195,8 @@ public class Movie {
                 ", languages='" + languages + '\'' +
                 ", runtime='" + runtime + '\'' +
                 ", anotherNames='" + anotherNames + '\'' +
-                ", IMDbLink='" + IMDbLink + '\'' +
+                ", imdbLink='" + imdbLink + '\'' +
+                ", coverLink='" + coverLink + '\'' +
                 ", synopsis='" + synopsis + '\'' +
                 ", stillsLink='" + stillsLink + '\'' +
                 ", stillsPhotosLinks='" + stillsPhotosLinks + '\'' +
