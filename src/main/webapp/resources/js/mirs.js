f418a6ea2e49201622c0c4b7a6642910b3ff3b8a
JiaXingBingGan <729869585@qq.com>
2016-11-06 13:23:15 +0800

    推荐首页修改

--- src/main/webapp/resources/js/mirs.js
+++ src/main/webapp/resources/js/mirs.js
@@ -1,77 +1,1 @@
-//业务逻辑代码
-
-var mirs = {
-
-    // 接口
-    URL : {
-        oAuthUrl: function (type) {
-            return '/oauth/' + type;
-        },
-        loginUrl: function () {
-            return '/accounts/login';
-        },
-        registerUrl: function () {
-            return '/accounts/register';
-        },
-        getCaptchaUrl: function () {
-            return '/captcha/get';
-        },
-        checkCaptchaUrl: function (captcha) {
-            return '/captcha/check/' + captcha;
-        },
-        checkUserEmailUrl: function (email) {
-            return '/inspection/userEmail?email=' + email;
-        },
-        checkUserNameUrl: function (name) {
-            return '/inspection/userName?name=' + name;
-        },
-        checkVerificationUrl: function (verification) {
-            return '/inspection/verification?verification=' + verification;
-        },
-        dailyMovieUrl: function () {
-            return '/movie/daily';
-        }
-
-    },
-
-    loadOAuthUrl: function (element, oauthUrl) {
-        $.post(oauthUrl, {}, function (result) {
-            if(result && result['success']) {
-                console.log(result['data']['url']);
-                $(element).attr("href", result['data']['url']);
-            }
-        })
-    },
-
-    changeCaptcha: function (element) {
-        $(element).hide().attr('src', this.URL.getCaptchaUrl() + '?' + Math.floor(Math.random()*100) ).fadeIn();
-    },
-
-    checkStatus: function (element, checkUrl, statusElement,id) {
-        $.post(checkUrl, {}, function (result) {
-            if(result && result['success']) {
-                $(statusElement).html("<span style='color: green'>√</span>");
-                $(id).removeAttr("disabled");
-            } else {
-                $(statusElement).html(result['error']);
-            }
-        })
-    },
-
-    getDailyMovie: function(statusElement,id) {
-        $.get(this.URL.dailyMovieUrl(), {}, function (result) {
-            if(result && result['success']) {
-                var movies = result['data'];
-                var parent = document.getElementById(statusElement);
-                for(var i = 0; i < movies.length; i++) {
-                    console.log(movies[i]['name']);
-                    var div = '<div style="width: 300px;height: 500px;margin-left:80px;margin-right:20px;margin-bottom:20px;background-color: #1F811F;float: left"><img src="'+movies[i]['coverLink']+'"></img>' +movies[i]['name']+ '</div>';
-                    $(statusElement).append(div);
-                }
-            } else {
-
-            }
-        })
-    }
-
-}
+//业务逻辑代码var mirs = {    // 接口    URL : {        oAuthUrl: function (type) {            return '/oauth/' + type;        },        loginUrl: function () {            return '/accounts/login';        },        registerUrl: function () {            return '/accounts/register';        },        getCaptchaUrl: function () {            return '/captcha/get';        },        checkCaptchaUrl: function (captcha) {            return '/captcha/check/' + captcha;        },        checkUserEmailUrl: function (email) {            return '/inspection/userEmail?email=' + email;        },        checkUserNameUrl: function (name) {            return '/inspection/userName?name=' + name;        },        checkVerificationUrl: function (verification) {            return '/inspection/verification?verification=' + verification;        },        dailyMovieUrl: function () {            return '/movie/daily';        }    },    loadOAuthUrl: function (element, oauthUrl) {        $.post(oauthUrl, {}, function (result) {            if(result && result['success']) {                console.log(result['data']['url']);                $(element).attr("href", result['data']['url']);            }        })    },    changeCaptcha: function (element) {        $(element).hide().attr('src', this.URL.getCaptchaUrl() + '?' + Math.floor(Math.random()*100) ).fadeIn();    },    checkStatus: function (element, checkUrl, statusElement,id) {        $.post(checkUrl, {}, function (result) {            if(result && result['success']) {                $(statusElement).html("<span style='color: green'>√</span>");                $(id).removeAttr("disabled");            } else {                $(statusElement).html(result['error']);            }        })    },    getDailyMovie: function(statusElement,id) {        $.get(this.URL.dailyMovieUrl(), {}, function (result) {            if(result && result['success']) {                var movies = result['data'];                var parent = document.getElementById(statusElement);                for(var i = 0; i < movies.length; i++) {                    console.log(movies[i]['name']);                    var movie_info = '<div class="cs"><p>'+movies[i]['name']+'</p></div>';                    var movie_title = '<strong><h5 style="text-align: center">' +movies[i]['name']+ '<br><label style="font-size: 12px">豆瓣 <span style="color: #5cb85c">'+movies[i]['doubanRating']+'</span>/IMDB <span style="color:red;">'+movies[i]['imdbRating']+'</span></label></h5></strong>';                    var div = '<div class="movie"><img width="300" height="444" src="'+movies[i]['coverLink']+'"></img>'+movie_title+movie_info+'</div>';                    $(statusElement).append(div);                }            } else {            }        })    }}
