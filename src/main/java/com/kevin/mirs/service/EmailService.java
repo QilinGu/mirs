7a7d90518a85848bb1888836b732686eb809aeab
邓 <1351650853@qq.com>
2016-11-23 18:27:49 +0800

    邮件服务

---
+++ src/main/java/com/kevin/mirs/service/EmailService.java
@@ -0,0 +1,14 @@
+package com.kevin.mirs.service;
+
+
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class EmailService {
+
+    public void sendEmail() {
+
+    }
+
+}
--- src/main/java/com/kevin/mirs/web/HomeController.java
+++ src/main/java/com/kevin/mirs/web/HomeController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
@@ -13,6 +14,7 @@ public class HomeController {
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
+    @ResponseBody
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index() {
         return "Restful services is up!";
